a6a8ddca29bc4122e3add2b7d84c395afc61df02
Caio <caiodib10@gmail.com>
2024-11-22 17:15:50 -0300

    Animação com Javascripto
    Animação com Javascripto

---
+++ recursos/javascript/animacao.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.section-animation');
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible'); // Adiciona a classe "visible"
+          // Não desative a observação para reativar as animações
+        } else {
+          entry.target.classList.remove('visible'); // Remove a classe quando sair do viewport
+        }
+      });
+    }, {
+      threshold: 0.2 // Ativa quando 20% da seção está visível
+    });
+  
+    sections.forEach(section => observer.observe(section));
+  });
